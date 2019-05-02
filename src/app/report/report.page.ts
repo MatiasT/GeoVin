b22f0010ece1607e0fed8423522471108a338796
Matias Teragni <matias.teragni@gmail.com>
2019-05-02 12:52:22 -0300

    Added a slider into the report page. This slider will be used as a wizard through the reporting process.

--- src/app/report/report.page.ts
+++ src/app/report/report.page.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-report',
   templateUrl: 'report.page.html',
   styleUrls: ['report.page.scss']
 })
-export class ReportPage {}
+export class ReportPage implements OnInit {
+  ngOnInit(): void {
+    
+    this.slider.lockSwipes(true);
+  }
+  // Optional parameters to pass to the swiper instance. See http://idangero.us/swiper/api/ for valid options.
+  @ViewChild("slider") slider:IonSlides;
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400
+  };
+  next(){
+    let self=this;
+    self.slider.lockSwipes(false).then(()=>{
+      self.slider.slideNext().then(()=>{self.slider.lockSwipes(true);});});
+  }
+  reset(){
+    let self=this;
+    self.slider.lockSwipes(false).then(()=>{
+      self.slider.slideTo(0).then(()=>{self.slider.lockSwipes(true);});});
+  }
+
+}
