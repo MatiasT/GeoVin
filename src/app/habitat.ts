ca5808c30fdbb0dd5110052774532c743df15e0c
Matias Teragni <matias.teragni@gmail.com>
2019-05-23 16:25:25 -0300

    Started using sightingReport as my internal representation of a report. Added a small class Habitat to encapsulate those fields, and possibly how i will display that information later on

---
+++ src/app/habitat.ts
@@ -0,0 +1,13 @@
+export class Habitat {
+    habitat: string;
+    subHabitat: string;
+    otherHabitat: string;
+    /**
+     *
+     */
+    constructor(habitat: string, subHabitat: string, otherHabitat: string) {
+        this.habitat = habitat;
+        this.subHabitat = subHabitat;
+        this.otherHabitat = otherHabitat;
+    }
+}
--- src/app/sightingReport.ts
+++ src/app/sightingReport.ts
@@ -1,10 +1,23 @@
+import { Habitat } from './habitat';
+
 export class sightingReport {
+    username:string;
+  datetime: Date;
+  firstPicture: string;
+  secondPicture: string;
+  lat: number;
+  lng: number;
+  habitat:Habitat;
     /**
      *
      */
     constructor() {
 
     }
+/*
+//this is the data i need to send.
+//since i need the full image path into fotoPath i think i wont have that data here.
+//i'll create another class to handle the communication
 
     username:string;
     deviceID:string;
@@ -21,4 +34,5 @@ export class sightingReport {
     wifidetect:any; //null?
     mapdetect:string; //si/null?
     verificado:any; //"no%20verificado"??
+    */
 }
