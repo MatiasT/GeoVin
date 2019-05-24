31f1d46f6c66b8cbdc8b562710f34e4891ea7d5e
Matias Teragni <matias.teragni@gmail.com>
2019-05-24 11:02:00 -0300

    Added the process to save the files. This does not work, i'm having issues with some paths. I'll get the image as a base64 instead

--- src/app/report/report.page.ts
+++ src/app/report/report.page.ts
@@ -3,7 +3,9 @@ import { IonSlides } from '@ionic/angular';
 import { Camera, CameraOptions } from "@ionic-native/camera/ngx";
 import { UIMapComponent } from '../uimap/uimap.component';
 import { ReportRepositoryService } from '../report-repository.service';
-
+import { File } from "@ionic-native/file/ngx";
+import { sightingReport } from '../sightingReport';
+import { Habitat } from '../habitat';
 const emptyImagePath = "assets/camera.svg";
 
 @Component({
@@ -17,20 +19,20 @@ export class ReportPage implements OnInit {
   firstImage: any;
   locationMarker: any;
   secondImage: any;
-  habitat:string;
-  subHabitat:string;
-  otherHabitat:string;
+  habitat: string;
+  subHabitat: string;
+  otherHabitat: string;
   /**
    *
    */
-  constructor(private camera: Camera, private repository:ReportRepositoryService) {
+  constructor(private camera: Camera, private repository: ReportRepositoryService, private file: File) {
     this.resetData();
-  } 
-  private resetData(){
+  }
+  private resetData() {
     this.resetImages();
-    this.habitat="domicilio";
-    this.subHabitat="";
-    this.otherHabitat="";
+    this.habitat = "domicilio";
+    this.subHabitat = "";
+    this.otherHabitat = "";
   }
   private resetImages() {
     this.firstImage = this.secondImage = emptyImagePath;
@@ -39,7 +41,8 @@ export class ReportPage implements OnInit {
     quality: 100,
     destinationType: this.camera.DestinationType.FILE_URI,
     encodingType: this.camera.EncodingType.JPEG,
-    mediaType: this.camera.MediaType.PICTURE
+    mediaType: this.camera.MediaType.PICTURE,
+    saveToPhotoAlbum: false
   }
   // Optional parameters to pass to the swiper instance. See http://idangero.us/swiper/api/ for valid options.
   @ViewChild("slider") slider: IonSlides;
@@ -58,7 +61,7 @@ export class ReportPage implements OnInit {
       this.locationMarker = this.map.AddCenteredMarker();
       this.map.map.on("move", (e) => {
         this.locationMarker
-        .setLatLng(this.map.map.getCenter()).update()
+          .setLatLng(this.map.map.getCenter()).update()
       });
     }
     //TODO: do this right?
@@ -84,20 +87,56 @@ export class ReportPage implements OnInit {
     // imageData is either a base64 encoded string or a file URI
     // If it's base64 (DATA_URL):
     // let base64Image = 'data:image/jpeg;base64,' + imageData;
-    console.log(imageData);
+
     return imageData;
     //TODO: error handling.
   }
-//habitat selection
+  //habitat selection
 
-habitatChanged(evt:any){
-  this.subHabitat="";
-  this.otherHabitat="";
-}
-habitatNotSelected(){
-  return this.subHabitat=="" || ( this.subHabitat=="otros" && this.otherHabitat=="");
-}
-//slider / wizard 
+  habitatChanged(evt: any) {
+    this.subHabitat = "";
+    this.otherHabitat = "";
+  }
+  habitatNotSelected() {
+    return this.subHabitat == "" || (this.subHabitat == "otros" && this.otherHabitat == "");
+  }
+
+  //wrap up repport
+  //save images to file.
+  async saveImage(imagePath: string, fileName: string) {
+    let currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
+    let correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
+    //this is failing because the paths are not well formed.
+    let testPath=await this.file.resolveLocalFilesystemUrl(imagePath);
+    let testFile = await this.file.readAsText(correctPath,currentName);
+    let entry = await this.file.copyFile(correctPath, currentName, this.file.dataDirectory, fileName);
+    return entry.fullPath;
+  }
+  private printDate(date: Date): String {
+    return [date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds()].join("_");
+  }
+  async commitReport() {
+    //i need to save the pictures taken into the filesystem
+    let report = new sightingReport();
+    report.datetime = new Date();
+
+    report.firstPicture = await this.saveImage(this.firstImage, report.username + this.printDate(report.datetime) + "_1.jpg");
+    report.secondPicture = await this.saveImage(this.firstImage, report.username + this.printDate(report.datetime) + "_2.jpg");
+    let location = this.locationMarker.getLatLng();
+    //TODO: use an object to encapsulate location?
+    report.lat=location.lat;
+    report.lng=location.lng
+    report.habitat=new Habitat(this.habitat,this.subHabitat,this.otherHabitat);
+    await this.repository.addReport(report);
+    await this.reset();
+  }
+
+  //slider / wizard 
   async next() {
     await this.slider.lockSwipes(false);
     await this.slider.slideNext();
