f576c1619c8c9cd99069809a152e0c73376063b2
Matias Teragni <matias.teragni@gmail.com>
2019-05-29 12:37:17 -0300

    Added some server information to the report. Now the view shows if the report has been uploaded or not.

--- src/app/storage/sightingReport.ts
+++ src/app/storage/sightingReport.ts
@@ -1,38 +1,42 @@
 import { Habitat } from './habitat';
 
 export class sightingReport {
-    username:string;
+  username: string;
   datetime: Date;
   firstPicture: string;
   secondPicture: string;
   lat: number;
   lng: number;
-  habitat:Habitat;
-    /**
-     *
-     */
-    constructor() {
+  habitat: Habitat;
 
-    }
-/*
-//this is the data i need to send.
-//since i need the full image path into fotoPath i think i wont have that data here.
-//i'll create another class to handle the communication
-
-    username:string;
-    deviceID:string;
-    datetime:Date;
-    lat:Number;
-    lng:Number;
-    valorVinchuca:string;
-    foto1Path:string;
-    foto2Path:string;
-    foto3Path:string;
-    foto4Path:string; //this one seems to hold the habitat.
-    privado:string; //si / no
-    gpsdetect:any; //null?
-    wifidetect:any; //null?
-    mapdetect:string; //si/null?
-    verificado:any; //"no%20verificado"??
-    */
+  reportID:Number;
+  verified:boolean;
+  /**
+   *
+   */
+  constructor() {
+    this.verified=false;
+    this.reportID=null;
+  }
+  /*
+  //this is the data i need to send.
+  //since i need the full image path into fotoPath i think i wont have that data here.
+  //i'll create another class to handle the communication
+  
+      username:string;
+      deviceID:string;
+      datetime:Date;
+      lat:Number;
+      lng:Number;
+      valorVinchuca:string;
+      foto1Path:string;
+      foto2Path:string;
+      foto3Path:string;
+      foto4Path:string; //this one seems to hold the habitat.
+      privado:string; //si / no
+      gpsdetect:any; //null?
+      wifidetect:any; //null?
+      mapdetect:string; //si/null?
+      verificado:any; //"no%20verificado"??
+      */
 }
