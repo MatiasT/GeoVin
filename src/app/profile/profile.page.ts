42bd902bf09a339ce642710680e29628e0c4ed9e
Matias Teragni <matias.teragni@gmail.com>
2019-05-29 11:28:38 -0300

    Now the profile tab shows the completed reports

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ReportRepositoryService } from '../storage/report-repository.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+  constructor(public repository: ReportRepositoryService) { }
 
   ngOnInit() {
+    
   }
 
+  
 }
