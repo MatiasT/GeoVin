538a95acae3071c2a88e2487c81270d572309951
Matias Teragni <matias.teragni@gmail.com>
2019-06-12 14:43:16 -0300

    Added some of the infrastructure to commit the reports

--- src/app/geovinAPI/geo-vin-api.service.ts
+++ src/app/geovinAPI/geo-vin-api.service.ts
@@ -1,18 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { locationReport } from './locationReport';
+import { ReportRepositoryService } from '../storage/report-repository.service';
+import { Network } from '@ionic-native/network/ngx';
 
+const sleepTime = 10000;
 @Injectable({
   providedIn: 'root'
 })
 export class GeoVinAPIService {
   baseURL: string = "http://www.geovin.com.ar/connect2";
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private repository: ReportRepositoryService, private network: Network) {
+
+    this.TryToSendReports();
+  }
+  /*
+  This method gets the pending data to be sent, the settings, and tryes to commit it to the server
+  */
+  private TryToSendReports() {
+    let settings = this.repository.getSettings();
+    if (!settings.commitOverWifi || //if the settings say i can commit over anything, or i can commit over wifi only and i am connected to wifi
+      (settings.commitOverWifi && this.network.type == this.network.Connection.WIFI)) {
+      this.repository.getPendingReports().forEach(report => {
+          //TODO: commit the report.
+          //TODO: update the report?
+      });
+
+    }
+    setTimeout(this.TryToSendReports, sleepTime);
+  }
 
 
 
-  public getReportJson(data: string) {
+  private getReportJson(data: string) {
     //the return of the service is not a valid json.
     //first of all i need to strip that header
     data = data.slice(data.indexOf('{'));
