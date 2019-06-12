cdfb7716666f4f351e0ee40e4bae8b1b6b4b703f
Matias Teragni <matias.teragni@gmail.com>
2019-06-12 13:35:26 -0300

    Added first version of the fcm service. For now it only  gets the token

---
+++ src/app/fcm.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {FCM} from '@ionic-native/fcm/ngx'
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FCMService {
+  private token: string="";
+  
+  constructor(private fcm:FCM) {
+    fcm.getToken().then((token)=>{
+      this.token=token;
+    });
+    fcm.onTokenRefresh().subscribe(token=>{this.token=token});
+    //TODO: subscribe to topic.
+    //fcm.subscribeToTopic()
+    
+    //TODO use output and eventemmiter to trigger refresh on the notification.
+    //fcm.onNotification()
+   }
+
+   public getToken():string
+   {
+     return this.token;
+   }
+}
