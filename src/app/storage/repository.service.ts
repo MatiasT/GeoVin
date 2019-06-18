def09e0732ca162f09e2d571ec26834e3a19c4cb
Matias Teragni <matias.teragni@gmail.com>
2019-06-18 17:02:26 -0300

    Added an init function, and a proimse to represent if the storage is ready Instead of loading all in the constructor now an init function handles it using async/await.

--- src/app/storage/repository.service.ts
+++ src/app/storage/repository.service.ts
@@ -14,58 +14,58 @@ export class RepositoryService {
   private reports: sightingReport[];
   private settings: Settings;
   private user: User;
-  
-  private readyPromise:Promise<any>;
-  public get ready() : Promise<any> {
+
+  private readyPromise: Promise<any>;
+  public get ready(): Promise<any> {
     return this.readyPromise;
   }
-  
+
 
   constructor(private storage: NativeStorage, private platform: Platform) {
     let self = this;
-    self.readyPromise=new Promise(
-      (res)=>{
-        platform.ready().then(() => {
-          //the storage does not hold type info, i'll create my own instances here.
-          self.storage.getItem('reports').then((data) => {
-            self.reports = data.map((d: any) => Object.assign(new sightingReport(), d));
-          }, (err) => {
-            console.error(err);
-            self.reports = new Array<sightingReport>();
-          });
-          self.storage.getItem('settings').then(
-            (data) => { self.settings = Object.assign(new Settings(), data); },
-            (err) => { console.error(err); self.settings = new Settings() });
-          res(self);
-        });
-
+    self.readyPromise = new Promise(
+      (res) => {
+        self.init().then(() => res());
       });
   }
+  private async init(): Promise<RepositoryService> {
+    await this.platform.ready();
+    //the storage does not hold type info, i'll create my own instances here.
+    let keys:Array<string>= await this.storage.keys();
+    if(!keys.includes("reports")){
+      await this.storage.setItem("reports",new Array<sightingReport>());
+    }
+    this.reports = (await this.storage.getItem('reports')).map((d: any) => Object.assign(new sightingReport(), d));
+    if(!keys.includes("settings")){
+      await this.storage.setItem("settings",new Settings());
+    }
+    this.settings = Object.assign(new Settings(), await this.storage.getItem('settings'));
+    return this;
+}
+//TODO: make the getters async.
+getUser() {
+  if (!this.user) return User.EmptyUser;
+  return this.user;
+}
+getReports(): sightingReport[] {
+  return this.reports;
+}
+getPendingReports(): sightingReport[] {
+  return this.getReports().filter(report => report.state == "Pending");
+}
+async addReport(report: sightingReport) {
+  this.reports.push(report);
+  await this.updateReports();
+}
 
-  //TODO: make the getters async.
-  getUser() {
-    if (!this.user) return User.EmptyUser;
-    return this.user;
-  }
-  getReports(): sightingReport[] {
-    return this.reports;
-  }
-  getPendingReports(): sightingReport[] {
-    return this.getReports().filter(report => report.state == "Pending");
-  }
-  async addReport(report: sightingReport) {
-    this.reports.push(report);
-    await this.updateReports();
-  }
-
-  getSettings(): Settings {
-    return this.settings;
-  }
-  async updateSettings(settings: Settings) {
-    this.settings = settings;
-    await this.storage.setItem("settings", this.settings);
-  }
-  async updateReports() {
-    await this.storage.setItem("reports", this.reports)
-  }
+getSettings(): Settings {
+  return this.settings;
+}
+async updateSettings(settings: Settings) {
+  this.settings = settings;
+  await this.storage.setItem("settings", this.settings);
+}
+async updateReports() {
+  await this.storage.setItem("reports", this.reports)
+}
 }
