488683575aa710fb158dda778f9e00eb49238047
Matias Teragni <matias.teragni@gmail.com>
2019-06-26 11:55:15 -0300

    Added the link list into learn. Had to add a domsanitizer to go around a security validation of angular. Warning: This should only be used to address trusted local files!

--- src/app/learn/learn.page.ts
+++ src/app/learn/learn.page.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-learn',
   templateUrl: 'learn.page.html',
   styleUrls: ['learn.page.scss']
 })
-export class LearnPage {}
+export class LearnPage {
+  @Output()
+  resourcePath: string;
+  
+  constructor(public sanitizer:DomSanitizer) {
+      this.resourcePath=null;
+  }
+
+}
